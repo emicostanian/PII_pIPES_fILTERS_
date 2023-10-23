e0740b4a3793901b98b7ea3c810adf1798b02a1d
emicostanian <mariaemiliacostanian@gmail.com>
2023-10-23 10:13:33 -0300

    program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,12 +2,27 @@
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
 
+
 namespace CompAndDel
 {
     class Program
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"lando.jpg");
+
+            //pipes y filters
+            IFilter filter1 = new FilterGreyscale();
+            IFilter filter2 = new FilterNegative();
+
+            IPipe pipe1 = new PipeSerial(filter1, new PipeNull());
+            IPipe pipe2 = new PipeSerial(filter2, new PipeNull());
+
+            pipe1.Send(picture);
+            IPicture picture1 = pipe2.Send(picture);
+            
+            provider.SavePicture(picture1, @"lando.jpg");
         }
     }
 }
