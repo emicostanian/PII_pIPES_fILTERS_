ae11f01d25f939d8b33f1cb6259a7d9f53a75d29
emicostanian <mariaemiliacostanian@gmail.com>
2023-10-25 16:03:28 -0300

    agrego parte 1, 2, 3 y 4

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
-
+using Ucu.Poo.Twitter;
+using Ucu.Poo.Cognitive;
+using System.Drawing;
 
 namespace CompAndDel
 {
@@ -9,20 +11,77 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
-            PictureProvider provider = new PictureProvider();
-            IPicture picture = provider.GetPicture(@"lando.jpg");
-
-            //pipes y filters
-            IFilter filter1 = new FilterGreyscale();
-            IFilter filter2 = new FilterNegative();
-
-            IPipe pipe1 = new PipeSerial(filter1, new PipeNull());
-            IPipe pipe2 = new PipeSerial(filter2, new PipeNull());
-
-            pipe1.Send(picture);
-            IPicture picture1 = pipe2.Send(picture);
+            // //PARTE1
             
-            provider.SavePicture(picture1, @"lando.jpg");
+            //PictureProvider provider = new PictureProvider();
+            //IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            // //pipes y filters
+            // IFilter filter1 = new FilterGreyscale();
+            // IFilter filter2 = new FilterNegative();
+
+            // IPipe pipe1 = new PipeSerial(filter1, new PipeNull());
+            // IPipe pipe2 = new PipeSerial(filter2, pipe1);
+
+            // pipe1.Send(picture);
+            // IPicture picture1 = pipe2.Send(picture);
+            
+            
+            // provider.SavePicture(picture1, @"editada3.jpg");
+
+            // //PARTE2
+
+            // PictureProvider provider = new PictureProvider();
+            // IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            // //pipes y filters
+            // IFilter filter1 = new FilterGreyscale();
+            // IFilter filter2 = new FilterNegative();
+
+            // IPipe pipe1 = new PipeSerial(filter1, new PipeNull());
+            // IPipe pipe2 = new PipeSerial(filter2, new PipeNull());
+
+            // pipe1.Send(picture);
+            // IPicture picture1 = pipe1.Send(picture);
+
+            // pipe2.Send(picture);
+            // IPicture picture2 = pipe2.Send(picture);
+            
+            
+            // provider.SavePicture(picture1, @"editada1.jpg");
+            // provider.SavePicture(picture2, @"editada2.jpg");
+
+            // //PARTE3
+
+            //var twitter = new TwitterImage();
+            //Console.WriteLine(twitter.PublishToTwitter("EMI", @"editada.jpg"));
+            
+            // //PARTE 4 
+
+            {
+                CognitiveFace cog = new CognitiveFace(true, Color.GreenYellow); //no reconoce la cara porque no esta a color
+                cog.Recognize(@"lando.jpg");
+                FoundFace(cog);
+            }
+
+            static void FoundFace(CognitiveFace cog)
+            {
+                if (cog.FaceFound)
+                {
+                    Console.WriteLine("Face Found!");
+                    if (cog.GlassesFound)
+                    {
+                        Console.WriteLine("Has glasses 🤓");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No glasses");
+                    }
+                }
+                else
+                    Console.WriteLine("No Face Found");
+                }
+            }
         }
     }
-}
+
